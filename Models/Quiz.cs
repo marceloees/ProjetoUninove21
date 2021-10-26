c68ad32b0c4575d02c26be297e08aa5df769f909
marceloees <marceloees17@gmail.com>
2021-10-25 21:11:50 -0300

    Commit login

---
+++ Models/Quiz.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoQuiz.Models
+{
+    public class Quiz
+    {
+        public int Id {get;set;}
+
+        public string User { get; set; }
+
+        public int Pontuacao { get; set; }
+    }
+}
